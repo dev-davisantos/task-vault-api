390fc3e7a6b35a4385842c364be32ebed1d1e174
Davi Santos <davicoutinho984@gmail.com>
2026-09-10 09:29:18 -0300

    feat: create a simple security configuration and a null auth controller for spring security implementation

---
+++ src/main/java/dev/davisantos/TaskVaultApi/config/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package dev.davisantos.TaskVaultApi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer:: disable)
+                .authorizeHttpRequests(authorization -> {
+                    authorization.requestMatchers("auth/**").permitAll();
+                    authorization.anyRequest().authenticated();
+                })
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(5);
+    }
+}
